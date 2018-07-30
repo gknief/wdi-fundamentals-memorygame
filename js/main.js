aa74216d9babc7cd58e730d5351ad5d4d82ce90a
Grant Knief <Grant@Grants-MacBook-Pro.local>
2018-07-29 20:50:19 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -30,7 +30,9 @@ var checkForMatch = function() {
 	console.log("Sorry, try again.");
 	}
 }
-var flipCard = function(cardId) {
+var flipCard = function() {
+	var cardId = this.getAttribute('data-id');
+	this.setAttribute('src', cards[cardId].cardImage);
 	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
@@ -38,7 +40,16 @@ var flipCard = function(cardId) {
 	checkForMatch();
 }
 
-flipCard(0);
-flipCard(2);
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+
+createBoard();
 
 
